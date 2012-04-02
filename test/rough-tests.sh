a1e5f7284f2492442fb7f4b11a7bbd6636f3bd5d
Flammie A Pirinen <ffflammie@gmail.com>
2012-04-02 15:39:29 +0300

    Test that word list is available, extra_dist it

--- test/rough-tests.sh
+++ test/rough-tests.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
+if test ! -r wordforms.list ; then
+    echo Missing word list wordforms.list
+    exit 73
+fi
 hfst-lookup ../src/morphology.omor.hfst < wordforms.list > wordforms.anals
 if grep '+?' wordforms.anals ; then
     echo "following known wordforms were missing from ../src/morphology.omor.hfst:"
