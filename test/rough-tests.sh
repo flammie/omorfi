cf1a8c6578b047b60d91c723e844e9eca38c10f8
Flammie Pirinen <flammie@iki.fi>
2012-12-26 10:11:17 +0200

    Use $srcdir for VPATH builds

--- test/rough-tests.sh
+++ test/rough-tests.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
-if test ! -r wordforms.list ; then
-    echo Missing word list wordforms.list
+if test ! -r $srcdir/wordforms.list ; then
+    echo Missing word list $srcdir/wordforms.list
     exit 73
 fi
-hfst-lookup ../src/morphology.omor.hfst < wordforms.list > wordforms.anals
+hfst-lookup ../src/morphology.omor.hfst < $srcdir/wordforms.list > wordforms.anals
 if grep '+?' wordforms.anals ; then
     echo "following known wordforms were missing from ../src/morphology.omor.hfst:"
     grep '+?' wordforms.anals
