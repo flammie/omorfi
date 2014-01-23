5e3fd9ed03c294055b910163aeaf959229e55ff8
Flammie Pirinen <ffFlammie@gmail.com>
2014-01-23 02:33:02 +0200

    Nifty script for adding new words to db

---
+++ src/lexical/add-word.bash
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+LEXFILE=lexemes/lexemes.tsv
+
+if test $# -lt 2 ; then
+    echo "Usage: $0 LEMMA CLASS"
+    exit 1
+fi
+
+cp -v ${LEXFILE} ${LEXFILE}~
+
+case $2 in
+    [0-9]*-[A-Z]) python guess_missing.py --lemma=$1 --ktn=${2%-*} --kav=${2#*-} --output=${LEXFILE} --verbose;;
+    [0-9]*) python guess_missing.py --lemma=$1 --ktn=$2 --output=${LEXFILE} --verbose;;
+    [A-Z]*_*) python guess_missing.py --lemma=$1 --newpara=$2 --output=${LEXFILE} --verbose;;
+esac
+
+sort ${LEXFILE} | uniq > ${LEXFILE}~~
+diff -u ${LEXFILE}~ ${LEXFILE}~~
+cp ${LEXFILE}~~ ${LEXFILE}
+echo Abovementioned addition was made, old version is still in ${LEXFILE}~
+echo if all is fine, do commit the result immediately
