de4bded4aecb90e120ffaaa38b333839cf215b08
Juha Kuokkala <juha.kuokkala@helsinki.fi>
2014-06-16 20:42:06 +0300

    Automatically convert precomposed 51.lexc to ftb3 format

--- src/scripts/convert_tag_format.py
+++ src/scripts/convert_tag_format.py
@@ -2,186 +2,196 @@
 #
 # utils to convert all tag formats with omor as pivot
 
-omor2ftb3 = {
-        '[WORD_ID=': '',
-        '[SUBCAT=ADJECTIVE]': ' A',
-        '[POS=VERB]': ' V',
-        '[POS=NOUN]': ' N',
-        '[POS=PARTICLE]': '',
-        '[SUBCAT=PRONOUN]': ' Pron',
-        '[SUBCAT=NUMERAL]': ' Num',
-        '[SUBCAT=PROPER]': ' Prop',
-        '[SUBCAT=ADVERB]': ' Adv',
-        '[SUBCAT=ADPOSITION]': '',
-        '[SUBCAT=QUALIFIER]': ' A',
-        '[SUBCAT=INTERJECTION]': ' Interj',
-        '[SUBCAT=DEMONSTRATIVE]': ' Dem',
-        '[SUBCAT=PERSONAL]': ' Pers',
-        '[SUBCAT=INTERROGATIVE]': ' Interr',
-        '[SUBCAT=RELATIVE]': ' Rel',
-        '[SUBCAT=QUANTOR]': ' Qu',
-        '[SUBCAT=REFLEXIVE]': ' Reflex',
-        '[SUBCAT=RECIPROCAL]': ' Rec',
-        '[SUBCAT=INDEFINITE]': ' Ind',
-        '[SUBCAT=CARDINAL]': '',
-        '[SUBCAT=ORDINAL]': ' Ord',
-        '[SUBCAT=CONJUNCTION]': '',
-        '[SUBCAT=COORDINATING]': ' CC',
-        '[SUBCAT=ADVERBIAL]': ' CS',
-        '[SUBCAT=COMPARATIVE]': ' Comp',
-        '[SUBCAT=POSTPOSITION]': ' Po',
-        '[SUBCAT=PREPOSITION]': ' Pr',
-        '[SUBCAT=PREFIX]': '',
-        '[SUBCAT=SUFFIX]': '',
-        '[SUBCAT=ABBREVIATION]': ' Abbr',
-        '[SUBCAT=ACRONYM]': ' Abbr',
-        '[SUBCAT=PUNCTUATION]': ' Punct',
-        '[SUBCAT=SYMBOL]': '',
-        '[SUBCAT=SPACE]': '',
-        '[SUBCAT=QUOTATION]': '',
-        '[SUBCAT=BRACKET]': '',
-        '[SUBCAT=DASH]': '',
-        '[SUBCAT=CURRENCY]': '',
-        '[SUBCAT=MATH]': '',
-        '[SUBCAT=OPERATION]': '',
-        '[SUBCAT=RELATION]': '',
-        '[SUBCAT=INITIAL]': '',
-        '[SUBCAT=FINAL]': '',
-        '[CASE=NOMINATIVE]': ' Nom',
-        '[CASE=PARTITIVE]': ' Par',
-        '[CASE=GENITIVE]': ' Gen',
-        '[CASE=INESSIVE]': ' Ine',
-        '[CASE=ELATIVE]': ' Ela',
-        '[CASE=ILLATIVE]': ' Ill',
-        '[CASE=ADESSIVE]': ' Ade',
-        '[CASE=ABLATIVE]': ' Abl',
-        '[CASE=ALLATIVE]': ' All',
-        '[CASE=ESSIVE]': ' Ess',
-        '[CASE=INSTRUCTIVE]': ' Ins',
-        '[CASE=ABESSIVE]': ' Abe',
-        '[CASE=TRANSLATIVE]': ' Tra',
-        '[CASE=COMITATIVE]': ' Com',
-        '[CASE=LATIVE]': ' Lat',
-        '[CASE=ACCUSATIVE]': ' Acc',
-        '[NUMBER=SINGULAR]': ' Sg',
-        '[NUMBER=PLURAL]': ' Pl',
-        '[POSSESSIVE=1STSINGULAR]': ' PxSg1',
-        '[POSSESSIVE=2NDSINGULAR]': ' PxSg2',
-        '[POSSESSIVE=3RDSINGULAR]': ' PxSg3',
-        '[POSSESSIVE=1STPLURAL]': ' PxPl1',
-        '[POSSESSIVE=2NDPLURAL]': ' PxPl2',
-        '[POSSESSIVE=3RDPLURAL]': ' PxPl3',
-        '[POSSESSIVE=3RDAMBIGUOUS]': ' Px3',
-        '[BOUNDARY=COMPOUND]': '#',
-        '[COMPOUND_FORM=S]': '',
-        '[COMPOUND_FORM=OMIT]': ' TrunCo', 
-        '[TENSEMOOD=PRESENTINDICATIVE]': ' Prt',
-        '[TENSEMOOD=PASTINDICATIVE]': ' Pst',
-        '[TENSEMOOD=CONDITIONAL]': ' Cond',
-        '[TENSEMOOD=POTENTIAL]': ' Pot',
-        '[TENSEMOOD=IMPERATIVE]': ' Imp',
-        '[TENSEMOOD=OPTATIVE]': ' Opt',
-        '[TENSEMOOD=EVENTIVE]': '',
-        '[PERSON=1STSINGULAR]': ' Sg1',
-        '[PERSON=2NDSINGULAR]': ' Sg2',
-        '[PERSON=3RDSINGULAR]': ' Sg3',
-        '[PERSON=1STPLURAL]': ' Pl1',
-        '[PERSON=2NDPLURAL]': ' Pl2',
-        '[PERSON=3RDPLURAL]': ' Pl3',
-        '[PERSON=IMPERSONAL]': ' Pe4',
-        '[NEGATION=CON]': ' ConNeg',
-        '[SUBCAT=NEGATIONVERB]': ' Neg',
-        '[VOICE=ACTIVE]': ' Act',
-        '[VOICE=PASSIVE]': ' Pass',
-        '[INFINITIVE=A]': ' Inf1',
-        '[INFINITIVE=E]': ' Inf2',
-        '[INFINITIVE=MA]': ' Inf3',
-        '[DERIVATION=MINEN]': ' Inf4',
-        '[DERIVATION=MAISILLA]': ' Inf5',
-        '[PARTICIPLE=NUT]': ' PrfPrc',
-        '[PARTICIPLE=AGENT]': ' AgPrc',
-        '[PARTICIPLE=VA]': ' PrtPrc',
-        '[PARTICIPLE=NEGATION]': 'NegPrc',
-        '[DERIVATION=NUT]': '',
-        '[DERIVATION=TU]': '',
-        '[DERIVATION=MA]': '',
-        '[DERIVATION=VA]': '',
-        '[DERIVATION=MATON]': '',
-        '[COMPARISON=POSITIVE]': ' Pos',
-        '[COMPARISON=COMPARATIVE]': ' Comp',
-        '[COMPARISON=SUPERLATIVE]': ' Super',
-        '[DERIVATION=MPI]': '',
-        '[DERIVATION=IN]': '', 
-        '[CLITIC=HAN]': ' Foc_hAn',
-        '[CLITIC=KAAN]': ' Foc_kAAn',
-        '[CLITIC=KIN]': ' Foc_kin',
-        '[CLITIC=KO]': ' Foc_kO',
-        '[CLITIC=PA]': ' Foc_pA',
-        '[CLITIC=S]': ' Foc_s',
-        '[CLITIC=KA]': ' Foc_kA',
-        '[DERIVATION=STI]': ' Man', 
-        '[DERIVATION=JA]': '',
-        '[DERIVATION=INEN]': '', 
-        '[DERIVATION=LAINEN]': '',
-        '[DERIVATION=TAR]': '',
-        '[DERIVATION=LLINEN]': '',
-        '[DERIVATION=TON]': '',
-        '[DERIVATION=TSE]': '',
-        '[DERIVATION=OI]':'',
-        '[DERIVATION=VS]': '',
-        '[DERIVATION=U]': '',
-        '[DERIVATION=TTAIN]': '',
-        '[DERIVATION=TTAA]': '',
-        '[DERIVATION=TATTAA]': '',
-        '[DERIVATION=TATUTTAA]': '',
-        '[DERIVATION=UUS]': '',
-        '[DERIVATION=S]': '',
-        '[DERIVATION=NUT]': '',
-        '[STYLE=NONSTANDARD]': '',
-        '[STYLE=RARE]': '',
-        '[STYLE=DIALECTAL]': '',
-        '[STYLE=ARCHAIC]': '',
-        '[GUESS=COMPOUND]': '',
-        '[GUESS=DERIVE]': '',
-        '[ALLO=A]': '',
-        '[ALLO=TA]': '',
-        '[ALLO=HVN]': '',
-        '[ALLO=IA]': '',
-        '[ALLO=IDEN]': '',
-        '[ALLO=ITA]': '',
-        '[ALLO=ITTEN]': '',
-        '[ALLO=IEN]': '',
-        '[ALLO=IHIN]': '',
-        '[ALLO=IIN]': '',
-        '[ALLO=IN]': '',
-        '[ALLO=ISIIN]': '',
-        '[ALLO=IDEN]': '',
-        '[ALLO=JA]': '',
-        '[ALLO=JEN]': '',
-        '[ALLO=SEEN]': '',
-        '[ALLO=TEN]': '',
-        '[ALLO=VN]': '',
-        '[FILTER=NO_PROC]': '',
-        '[PROP=FIRST]': '',
-        '[PROP=GEO]': '',
-        '[PROP=LAST]': '',
-        '[PROP=MISC]': '',
-        '[PROP=ORG]': '',
-        '[PROP=PRODUCT]': '',
-        '[PROP=EVENT]': '',
-        '[PROP=MEDIA]': '',
-        '[PROP=CULTGRP]': '',
-        '[PROP=ARTWORK]': '',
-        '[SEM=TITLE]': '',
-        '[SEM=ORG]': '',
-        '[SEM=EVENT]': '',
-        '[SEM=POLIT]': '',
-        '[SEM=MEDIA]': '',
-        '[SEM=GEO]': '',
-        '[SEM=MEASURE]': '',
-        '[SEM=CURRENCY]': '',
-        '[SEM=TIME]': ''}
+import sys, os, re
+import argparse
+from collections import defaultdict
 
+sys.path.append(os.path.join(os.path.dirname(__file__), '..', 'lexical'))
+import format_output
+
+# dig conversion tables from format_output module to stay up to date
+omor2ftb3 = { 
+        # '[WORD_ID=': '',
+        # '[SUBCAT=ADJECTIVE]': ' A',
+        # '[POS=VERB]': ' V',
+        # '[POS=NOUN]': ' N',
+        # '[POS=PARTICLE]': '',
+        # '[SUBCAT=PRONOUN]': ' Pron',
+        # '[SUBCAT=NUMERAL]': ' Num',
+        # '[SUBCAT=PROPER]': ' Prop',
+        # '[SUBCAT=ADVERB]': ' Adv',
+        # '[SUBCAT=ADPOSITION]': '',
+        # '[SUBCAT=QUALIFIER]': ' A',
+        # '[SUBCAT=INTERJECTION]': ' Interj',
+        # '[SUBCAT=DEMONSTRATIVE]': ' Dem',
+        # '[SUBCAT=PERSONAL]': ' Pers',
+        # '[SUBCAT=INTERROGATIVE]': ' Interr',
+        # '[SUBCAT=RELATIVE]': ' Rel',
+        # '[SUBCAT=QUANTOR]': ' Qu',
+        # '[SUBCAT=REFLEXIVE]': ' Reflex',
+        # '[SUBCAT=RECIPROCAL]': ' Rec',
+        # '[SUBCAT=INDEFINITE]': ' Ind',
+        # '[SUBCAT=CARDINAL]': '',
+        # '[SUBCAT=ORDINAL]': ' Ord',
+        # '[SUBCAT=CONJUNCTION]': '',
+        # '[SUBCAT=COORDINATING]': ' CC',
+        # '[SUBCAT=ADVERBIAL]': ' CS',
+        # '[SUBCAT=COMPARATIVE]': ' Comp',
+        # '[SUBCAT=POSTPOSITION]': ' Po',
+        # '[SUBCAT=PREPOSITION]': ' Pr',
+        # '[SUBCAT=PREFIX]': '',
+        # '[SUBCAT=SUFFIX]': '',
+        # '[SUBCAT=ABBREVIATION]': ' Abbr',
+        # '[SUBCAT=ACRONYM]': ' Abbr',
+        # '[SUBCAT=PUNCTUATION]': ' Punct',
+        # '[SUBCAT=SYMBOL]': '',
+        # '[SUBCAT=SPACE]': '',
+        # '[SUBCAT=QUOTATION]': '',
+        # '[SUBCAT=BRACKET]': '',
+        # '[SUBCAT=DASH]': '',
+        # '[SUBCAT=CURRENCY]': '',
+        # '[SUBCAT=MATH]': '',
+        # '[SUBCAT=OPERATION]': '',
+        # '[SUBCAT=RELATION]': '',
+        # '[SUBCAT=INITIAL]': '',
+        # '[SUBCAT=FINAL]': '',
+        # '[CASE=NOMINATIVE]': ' Nom',
+        # '[CASE=PARTITIVE]': ' Par',
+        # '[CASE=GENITIVE]': ' Gen',
+        # '[CASE=INESSIVE]': ' Ine',
+        # '[CASE=ELATIVE]': ' Ela',
+        # '[CASE=ILLATIVE]': ' Ill',
+        # '[CASE=ADESSIVE]': ' Ade',
+        # '[CASE=ABLATIVE]': ' Abl',
+        # '[CASE=ALLATIVE]': ' All',
+        # '[CASE=ESSIVE]': ' Ess',
+        # '[CASE=INSTRUCTIVE]': ' Ins',
+        # '[CASE=ABESSIVE]': ' Abe',
+        # '[CASE=TRANSLATIVE]': ' Tra',
+        # '[CASE=COMITATIVE]': ' Com',
+        # '[CASE=LATIVE]': ' Lat',
+        # '[CASE=ACCUSATIVE]': ' Acc',
+        # '[NUMBER=SINGULAR]': ' Sg',
+        # '[NUMBER=PLURAL]': ' Pl',
+        # '[POSSESSIVE=1STSINGULAR]': ' PxSg1',
+        # '[POSSESSIVE=2NDSINGULAR]': ' PxSg2',
+        # '[POSSESSIVE=3RDSINGULAR]': ' PxSg3',
+        # '[POSSESSIVE=1STPLURAL]': ' PxPl1',
+        # '[POSSESSIVE=2NDPLURAL]': ' PxPl2',
+        # '[POSSESSIVE=3RDPLURAL]': ' PxPl3',
+        # '[POSSESSIVE=3RDAMBIGUOUS]': ' Px3',
+        # '[BOUNDARY=COMPOUND]': '#',
+        # '[COMPOUND_FORM=S]': '',
+        # '[COMPOUND_FORM=OMIT]': ' TrunCo', 
+        # '[TENSEMOOD=PRESENTINDICATIVE]': ' Prt',
+        # '[TENSEMOOD=PASTINDICATIVE]': ' Pst',
+        # '[TENSEMOOD=CONDITIONAL]': ' Cond',
+        # '[TENSEMOOD=POTENTIAL]': ' Pot',
+        # '[TENSEMOOD=IMPERATIVE]': ' Imp',
+        # '[TENSEMOOD=OPTATIVE]': ' Opt',
+        # '[TENSEMOOD=EVENTIVE]': '',
+        # '[PERSON=1STSINGULAR]': ' Sg1',
+        # '[PERSON=2NDSINGULAR]': ' Sg2',
+        # '[PERSON=3RDSINGULAR]': ' Sg3',
+        # '[PERSON=1STPLURAL]': ' Pl1',
+        # '[PERSON=2NDPLURAL]': ' Pl2',
+        # '[PERSON=3RDPLURAL]': ' Pl3',
+        # '[PERSON=IMPERSONAL]': ' Pe4',
+        # '[NEGATION=CON]': ' ConNeg',
+        # '[SUBCAT=NEGATIONVERB]': ' Neg',
+        # '[VOICE=ACTIVE]': ' Act',
+        # '[VOICE=PASSIVE]': ' Pass',
+        # '[INFINITIVE=A]': ' Inf1',
+        # '[INFINITIVE=E]': ' Inf2',
+        # '[INFINITIVE=MA]': ' Inf3',
+        # '[DERIVATION=MINEN]': ' Inf4',
+        # '[DERIVATION=MAISILLA]': ' Inf5',
+        # '[PARTICIPLE=NUT]': ' PrfPrc',
+        # '[PARTICIPLE=AGENT]': ' AgPrc',
+        # '[PARTICIPLE=VA]': ' PrtPrc',
+        # '[PARTICIPLE=NEGATION]': 'NegPrc',
+        # '[DERIVATION=NUT]': '',
+        # '[DERIVATION=TU]': '',
+        # '[DERIVATION=MA]': '',
+        # '[DERIVATION=VA]': '',
+        # '[DERIVATION=MATON]': '',
+        # '[COMPARISON=POSITIVE]': ' Pos',
+        # '[COMPARISON=COMPARATIVE]': ' Comp',
+        # '[COMPARISON=SUPERLATIVE]': ' Super',
+        # '[DERIVATION=MPI]': '',
+        # '[DERIVATION=IN]': '', 
+        # '[CLITIC=HAN]': ' Foc_hAn',
+        # '[CLITIC=KAAN]': ' Foc_kAAn',
+        # '[CLITIC=KIN]': ' Foc_kin',
+        # '[CLITIC=KO]': ' Foc_kO',
+        # '[CLITIC=PA]': ' Foc_pA',
+        # '[CLITIC=S]': ' Foc_s',
+        # '[CLITIC=KA]': ' Foc_kA',
+        # '[DERIVATION=STI]': ' Man', 
+        # '[DERIVATION=JA]': '',
+        # '[DERIVATION=INEN]': '', 
+        # '[DERIVATION=LAINEN]': '',
+        # '[DERIVATION=TAR]': '',
+        # '[DERIVATION=LLINEN]': '',
+        # '[DERIVATION=TON]': '',
+        # '[DERIVATION=TSE]': '',
+        # '[DERIVATION=OI]':'',
+        # '[DERIVATION=VS]': '',
+        # '[DERIVATION=U]': '',
+        # '[DERIVATION=TTAIN]': '',
+        # '[DERIVATION=TTAA]': '',
+        # '[DERIVATION=TATTAA]': '',
+        # '[DERIVATION=TATUTTAA]': '',
+        # '[DERIVATION=UUS]': '',
+        # '[DERIVATION=S]': '',
+        # '[DERIVATION=NUT]': '',
+        # '[STYLE=NONSTANDARD]': '',
+        # '[STYLE=RARE]': '',
+        # '[STYLE=DIALECTAL]': '',
+        # '[STYLE=ARCHAIC]': '',
+        # '[GUESS=COMPOUND]': '',
+        # '[GUESS=DERIVE]': '',
+        # '[ALLO=A]': '',
+        # '[ALLO=TA]': '',
+        # '[ALLO=HVN]': '',
+        # '[ALLO=IA]': '',
+        # '[ALLO=IDEN]': '',
+        # '[ALLO=ITA]': '',
+        # '[ALLO=ITTEN]': '',
+        # '[ALLO=IEN]': '',
+        # '[ALLO=IHIN]': '',
+        # '[ALLO=IIN]': '',
+        # '[ALLO=IN]': '',
+        # '[ALLO=ISIIN]': '',
+        # '[ALLO=IDEN]': '',
+        # '[ALLO=JA]': '',
+        # '[ALLO=JEN]': '',
+        # '[ALLO=SEEN]': '',
+        # '[ALLO=TEN]': '',
+        # '[ALLO=VN]': '',
+        # '[FILTER=NO_PROC]': '',
+        # '[PROP=FIRST]': '',
+        # '[PROP=GEO]': '',
+        # '[PROP=LAST]': '',
+        # '[PROP=MISC]': '',
+        # '[PROP=ORG]': '',
+        # '[PROP=PRODUCT]': '',
+        # '[PROP=EVENT]': '',
+        # '[PROP=MEDIA]': '',
+        # '[PROP=CULTGRP]': '',
+        # '[PROP=ARTWORK]': '',
+        # '[SEM=TITLE]': '',
+        # '[SEM=ORG]': '',
+        # '[SEM=EVENT]': '',
+        # '[SEM=POLIT]': '',
+        # '[SEM=MEDIA]': '',
+        # '[SEM=GEO]': '',
+        # '[SEM=MEASURE]': '',
+        # '[SEM=CURRENCY]': '',
+        # '[SEM=TIME]': ''
+        }
+
+# TODO: omor tags should be updated here....
 omor2ftc = {
         '[WORD_ID=': '',
         '[SUBCAT=ADJECTIVE]': ' Adjective',
@@ -360,12 +370,27 @@ omor2ftc = {
         '[SEM=GEO]': '',
         '[SEM=MEASURE]': '',
         '[SEM=CURRENCY]': '',
-        '[SEM=TIME]': ''}
+        '[SEM=TIME]': ''
+        }
 
+
+def ensure_tables_loaded():
+    """
+    Build tag conversion tables from data of format_output module
+    if not done yet
+    """
+    if len(omor2ftb3) == 0:
+        stuff2ftb3 = defaultdict(lambda: '', format_output.stuff2ftb3)
+        # build map from reversed stuff2omor -> stuff2ftb3
+        for stuff, omor in format_output.stuff2omor.items():
+            omor2ftb3[omor] = stuff2ftb3[stuff]
+        # prevent surprises out of nowhere
+        omor2ftb3[''] = ''
 
 def convert_omor_tag(tag, fmt):
     """Convert single omor style tag in to target format
     """
+    ensure_tables_loaded()
     if fmt == 'omor':
         return tag
     elif fmt == 'ftc':
@@ -399,10 +424,28 @@ def convert_omor_string(omorstring, fmt):
         return _omor2ftc(omorstring)
     else:
         omors = omorstring.split('][')
-        conv = convert_omor_tag(omors[0] + ']')
-        for omor in omors[1:-1]:
+        omors[0] = omors[0].lstrip('[')
+        omors[-1] = omors[-1].rstrip(']')
+        
+        if fmt == 'ftb3':
+            last_wid = 0
+            # put case before number
+            for i in range(len(omors) - 1):
+                if omors[i].startswith('NUM=') and omors[i+1].startswith('CASE='):
+                    omors[i:i+2] = [omors[i+1], omors[i]]
+            # remove analysis tags from all but last compound part
+            new_omors = []
+            while 'BOUNDARY=COMPOUND' in omors:
+                tag = omors.pop(0)
+                if tag.startswith('BOUNDARY=COMPOUND') or tag.startswith('WORD_ID='):
+                    new_omors.append(tag)
+            new_omors.extend(omors)
+            omors = new_omors
+        
+        conv = ''
+        for omor in omors:
             conv += convert_omor_tag('[' + omor + ']', fmt)
-        conv += convert_omor_tag('[' + omors[-1])
+        return conv
 
 def _omor2ftc(omorstring):
     """
@@ -488,30 +531,61 @@ def _omor2ftc(omorstring):
     return ftc
 
 
-# self test
-if __name__ == '__main__':
-    fail = False
-    for stuff, omor in stuff2omor.items():
-        if len(omor) < 2:
-            continue
-        elif not omor in omor_multichars:
-            print("There are conflicting formattings in here!", omor, 
-                    "is not a valid defined omor multichar_symbol!")
-            fail = True
-    for stuff, omor in stuff2omor_short.items():
-        if len(omor) < 2:
-            continue
-        elif not omor in omor_short_multichars:
-            print("There are conflicting formattings in here!", omor, 
-                    "is not a valid defined omor-short multichar_symbol!")
-            fail = True
-    for stuff, ftb3 in stuff2ftb3.items():
-        if len(ftb3) < 2:
-            continue
-        elif not ftb3 in ftb3_multichars:
-            print("There are conflicting formattings in here!", ftb3, 
-                    "is not a valid defined ftb3 multichar_symbol!")
-            fail = True
-    if fail:
-        exit(1)
+def main():
+    ap = argparse.ArgumentParser(description=
+            "Convert between tag formats of Omorfi with omor as pivot")
+
+    ap.add_argument("--input", "-i", metavar="INFILE",
+            help="read data from INFILE")
+    ap.add_argument('--output', '-o', metavar="OUTFILE",
+            help="write converted stuff to OUTFILE")
+    ap.add_argument('--to-format', '-t', metavar="TO",
+            help="destination tag format", required=True)
+    ap.add_argument('--regex', '-r', metavar="REGEX",
+            help="convert only span matched by given regular expression")
+    ap.add_argument("--verbose", "-v", action="store_true", default=False,
+            help="print each step to stdout while processing")
+    ap.add_argument("--version", "-V", action="version")
+    args = ap.parse_args()
+
+    if args.input:
+        input = open(args.input, 'r', newline='')
+    else:
+        input = sys.stdin
+    if args.output:
+        output = open(args.output, 'w', newline='')
+    else:
+        output = sys.stdout
+    
+    if args.verbose:
+        ensure_tables_loaded()
+        print("#omor2ftb3 table: ", omor2ftb3)
+    
+    if args.regex:
+        re_convpatt = re.compile(args.regex)
+    
+    for line in input:
+        line = line.rstrip('\r\n')
+        if args.verbose: 
+            print("#input line:'%s'" %(line))
+        if args.regex:
+            match = re_convpatt.search(line)
+            if match:
+                span1 = line[: match.start()]
+                span2 = line[match.start() : match.end()]
+                span3 = line[match.end() :]
+                converted = span1 + convert_omor_string(span2, args.to_format) + span3
+            else:
+                converted = line
+        else:
+            converted = convert_omor_string(line, args.to_format)
+        print(converted, file=output)
+
+    input.close()
+    output.close()
+    exit()
+
+
+if __name__ == "__main__":
+    main()
 
