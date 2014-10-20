1f9d13eb64060b4188032d6817371025fb76606b
Juha Kuokkala <juha.kuokkala@helsinki.fi>
2014-10-20 18:40:05 +0300

    Rewrite add-lex script with bash arrays; warn about unrecognized attributes

--- src/lexical/add-lex-data.bash
+++ src/lexical/add-lex-data.bash
@@ -26,52 +26,44 @@ if [ ! -d "$TEMPDIR" ] ; then
   mkdir $TEMPDIR
 fi
 
+attrs=(     prono           subcat        bound      origin part             \
+  plt            pronu         prop           sem      style symbol         arg)
+attr_names=(subcat          subcat        boundaries origin particle         \
+  plt            stem-vowel    prop           sem      style symbol         argument)
+attr_files=(pronoun-classes subcategories boundaries origin particle-classes \
+  plurale-tantum pronunciation proper-classes semantic usage symbol-classes verb-arguments)
+
 file_stub=${add_file##*/}
 file_stub=$TEMPDIR/${file_stub/%.tsv}
+
 add_lex_file=${file_stub}.lex.tsv
-add_bound_file=${file_stub}.bound.tsv
-add_origin_file=${file_stub}.origin.tsv
-add_part_file=${file_stub}.part.tsv
-add_plt_file=${file_stub}.plt.tsv
-add_prono_file=${file_stub}.prono.tsv
-add_pronu_file=${file_stub}.pronu.tsv
-add_prop_file=${file_stub}.prop.tsv
-add_sem_file=${file_stub}.sem.tsv
-add_style_file=${file_stub}.style.tsv
-add_subcat_file=${file_stub}.subcat.tsv
-add_symbol_file=${file_stub}.symbol.tsv
-add_arg_file=${file_stub}.arg.tsv
+
+for (( i = 0 ; i < ${#attrs[@]} ; i++ )) ; do
+  add_attr_file[$i]=${file_stub}.${attrs[$i]}.tsv
+done
 
 PRON_SUBCATS='adjective|demonstrative|interrogative|personal|quantor|reciprocal|reflexive|relative'
 
 cut -f1,2 $add_file > $add_lex_file
-sed -r -n 's/^([^\t]*\t[^\t]*).*(\tboundaries=[^\t]*).*/\1\2/p' $add_file > $add_bound_file
-sed -r -n 's/^([^\t]*\t[^\t]*).*(\torigin=[^\t]*).*/\1\2/p' $add_file > $add_origin_file
-sed -r -n 's/^([^\t]*\t[^\t]*).*(\tparticle=[^\t]*).*/\1\2/p' $add_file > $add_part_file
-sed -r -n 's/^([^\t]*\t[^\t]*).*(\tplt=[^\t]*).*/\1\2/p' $add_file > $add_plt_file
-sed -r -n 's/^([^\t]*\t[^\t]*).*(\tstem-vowel=[^\t]*).*/\1\2/p' $add_file > $add_pronu_file
-sed -r -n 's/^([^\t]*\t[^\t]*).*(\tprop=[^\t]*).*/\1\2/p' $add_file > $add_prop_file
-sed -r -n 's/^([^\t]*\t[^\t]*).*(\tsem=[^\t]*).*/\1\2/p' $add_file > $add_sem_file
-sed -r -n 's/^([^\t]*\t[^\t]*).*(\tsymbol=[^\t]*).*/\1\2/p' $add_file > $add_symbol_file
-sed -r -n 's/^([^\t]*\t[^\t]*).*(\tstyle=[^\t]*).*/\1\2/p' $add_file > $add_style_file
-sed -r -n 's/^([^\t]*\t[^\t]*).*(\targument=[^\t]*).*/\1\2/p' $add_file > $add_arg_file
-sed -r -n "s/^([^\t]*\t[^\t]*).*(\tsubcat=(${PRON_SUBCATS})[^\t]*).*/\1\2/p" $add_file > $add_prono_file
-sed -r -n 's/^([^\t]*\t[^\t]*).*(\tsubcat=[^\t]*).*/\1\2/p' $add_file | egrep -v 'subcat=(${PRON_SUBCATS})' > $add_subcat_file
+sed -r -n "s/^([^\t]*\t[^\t]*).*(\tsubcat=(${PRON_SUBCATS})[^\t]*).*/\1\2/p" $add_file > ${add_attr_file[0]}
+sed -r -n 's/^([^\t]*\t[^\t]*).*(\tsubcat=[^\t]*).*/\1\2/p' $add_file | egrep -v 'subcat=(${PRON_SUBCATS})' > ${add_attr_file[1]}
+for (( i = 2 ; i < ${#attrs[@]} ; i++ )) ; do
+  sed -r -n "s/^([^\t]*\t[^\t]*).*(\t${attr_names[$i]}=[^\t]*).*/\1\2/p" $add_file > ${add_attr_file[$i]}
+done
+
+used_attrs=$(cut -f3- $add_file | tr '\t' '\n' | sed 's/=.*//g' | sort -u)
+LEGAL_ATTRS="${attr_names[*]}"
+LEGAL_ATTRS="${LEGAL_ATTRS// /|}|$PRON_SUBCATS"
+illegals=$(echo "$used_attrs" | egrep -v -x "$LEGAL_ATTRS")
+if [ -n "$illegals" ] ; then
+  illegals="$(echo ${illegals} | tr '\n' ' ')"
+  echo "WARNING: Ignoring unrecognized attribute(s): ${illegals}"
+fi
 
 lex_file="$LEXFILE"
-bound_file="${BASEDIR}/attributes/boundaries.tsv"
-origin_file="${BASEDIR}/attributes/origin.tsv"
-part_file="${BASEDIR}/attributes/particle-classes.tsv"
-plt_file="${BASEDIR}/attributes/plurale-tantum.tsv"
-prono_file="${BASEDIR}/attributes/pronoun-classes.tsv"
-pronu_file="${BASEDIR}/attributes/pronunciation.tsv"
-prop_file="${BASEDIR}/attributes/proper-classes.tsv"
-sem_file="${BASEDIR}/attributes/semantic.tsv"
-subcat_file="${BASEDIR}/attributes/subcategories.tsv"
-symbol_file="${BASEDIR}/attributes/symbol-classes.tsv"
-style_file="${BASEDIR}/attributes/usage.tsv"
-arg_file="${BASEDIR}/attributes/verb-arguments.tsv"
-
+for (( i = 0 ; i < ${#attrs[@]} ; i++ )) ; do
+  attr_file[$i]="${BASEDIR}/attributes/${attr_files[$i]}.tsv"
+done
 
 function join_tsv_data() {
   src_file="$1"
@@ -86,18 +78,9 @@ function join_tsv_data() {
 }
 
 join_tsv_data $add_lex_file $lex_file
-join_tsv_data $add_bound_file $bound_file
-join_tsv_data $add_origin_file $origin_file
-join_tsv_data $add_part_file $part_file
-join_tsv_data $add_plt_file $plt_file
-join_tsv_data $add_prono_file $prono_file
-join_tsv_data $add_pronu_file $pronu_file
-join_tsv_data $add_prop_file $prop_file
-join_tsv_data $add_sem_file $sem_file
-join_tsv_data $add_subcat_file $subcat_file
-join_tsv_data $add_symbol_file $symbol_file
-join_tsv_data $add_style_file $style_file
-join_tsv_data $add_arg_file $arg_file
+for (( i = 0 ; i < ${#attrs[@]} ; i++ )) ; do
+  join_tsv_data ${add_attr_file[$i]} ${attr_file[$i]}
+done
 
 echo Abovementioned files were modified, old versions are still in ${LEXFILE}~ etc.
 echo If all is fine, do commit the result immediately
