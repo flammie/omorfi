2089956ebfb10885752b65e02adca6f648e00419
Flammie Pirinen <flammie@iki.fi>
2015-01-12 05:16:39 +0000

    missed python file

---
+++ src/python/omorfi/__init__.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+"""Omorfi 2015 - Python interface to omorfi"""
+
+__all__ = ['Omorfi']
+
+__version__ = "2015"
+__author__ = "Omorfi contributors"
+__author_email__ = "omorfi-development@googlegroups.com"
+
+if __name__ == "__main__":
+    pass
+
+from .omorfi import Omorfi
