9d6a5e9c38c9215b7a288e6e440db8e7006f99c2
Flammie Pirinen <flammie@iki.fi>
2015-01-13 18:02:49 +0000

    allow stdin-stdout

--- src/python/omorfi-factorise.py
+++ src/python/omorfi-factorise.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-from sys import stdin
+from sys import stdin, stdout
 from argparse import ArgumentParser
 from omorfi.omorfi import Omorfi
 import re
@@ -14,7 +14,7 @@ def main():
             dest="infile", help="source of analysis data")
     a.add_argument('-v', '--verbose', action='store_true',
             help="print verbosely while processing")
-    a.add_argument('-o', '--output', metavar="OUTFILE", required=True,
+    a.add_argument('-o', '--output', metavar="OUTFILE", 
             help="print factors into OUTFILE")
     options = a.parse_args()
     omorfi = Omorfi(options.verbose)
@@ -26,8 +26,14 @@ def main():
         if options.verbose:
             print("Searching for automata everywhere...")
         omorfi.load_from_dir()
-    if not options.infile:
-        options.infile = stdin
+    if options.infile:
+        infile = open(options.infile)
+    else:
+        infile = stdin
+    if options.output:
+        outfile = open(options.output, 'w')
+    else:
+        outfile = stdout
     if options.verbose:
         print("reading from", options.infile.name)
     if options.verbose:
@@ -36,40 +42,43 @@ def main():
     re_lemma = re.compile("\[WORD_ID=([^]]*)\]")
     re_pos = re.compile("\[POS=([^]]*)\]")
     re_mrd = re.compile("\[([^=]*)=([^]]*)]")
-    with open(options.output, 'w') as outfile:
-        for line in options.infile:
-            line = line.strip()
-            if not line or line == '':
-                continue
-            surfs = line.split()
-            for surf in surfs:
-                anals = omorfi.analyse(surf)
-                segments = omorfi.segment(surf)
-                pos_matches = re_pos.finditer(anals[0].output)
-                pos = "UNK"
-                mrds = []
-                lemmas = []
-                for pm in pos_matches:
-                    pos = pm.group(1)
-                lemma_matches = re_lemma.finditer(anals[0].output)
-                for lm in lemma_matches:
-                    lemmas += [lm.group(1)]
-                mrd_matches = re_mrd.finditer(anals[0].output)
-                for mm in mrd_matches:
-                    if mm.group(1) == 'WORD_ID':
-                        mrds = []
-                    elif mm.group(1) == 'WEIGHT':
-                        pass
-                    else:
-                        mrds += [mm.group(2)]
-                stemfixes = segments[0].output[segments[0].output.rfind("{STUB}"):].replace("{STUB}", "")
-                if '{' in stemfixes:
-                    morphs = stemfixes[stemfixes.find("{"):].replace("{MB}", ".")
+    linen = 0
+    for line in infile:
+        line = line.strip()
+        linen += 1
+        if options.verbose and linen % 10000 == 0:
+            print(linen, '...')
+        if not line or line == '':
+            continue
+        surfs = line.split()
+        for surf in surfs:
+            anals = omorfi.analyse(surf)
+            segments = omorfi.segment(surf)
+            pos_matches = re_pos.finditer(anals[0].output)
+            pos = "UNK"
+            mrds = []
+            lemmas = []
+            for pm in pos_matches:
+                pos = pm.group(1)
+            lemma_matches = re_lemma.finditer(anals[0].output)
+            for lm in lemma_matches:
+                lemmas += [lm.group(1)]
+            mrd_matches = re_mrd.finditer(anals[0].output)
+            for mm in mrd_matches:
+                if mm.group(1) == 'WORD_ID':
+                    mrds = []
+                elif mm.group(1) == 'WEIGHT':
+                    pass
                 else:
-                    morphs = '0'
-                print(surf, '+'.join(lemmas), pos, '.'.join(mrds),
-                        morphs, sep='|', end=' ', file=outfile)
-            print(file=outfile)
+                    mrds += [mm.group(2)]
+            stemfixes = segments[0].output[segments[0].output.rfind("{STUB}"):].replace("{STUB}", "")
+            if '{' in stemfixes:
+                morphs = stemfixes[stemfixes.find("{"):].replace("{MB}", ".")
+            else:
+                morphs = '0'
+            print(surf, '+'.join(lemmas), pos, '.'.join(mrds),
+                    morphs, sep='|', end=' ', file=outfile)
+        print(file=outfile)
     exit(0)
 
 if __name__ == "__main__":
