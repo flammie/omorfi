c3dc69472e4b288b47a1896b1c6773f86af31089
Flammie Pirinen <flammie@iki.fi>
2015-03-19 17:17:58 +0000

    fixed add-word.bash again
    had to delete lost guessing code??

--- src/add-word.bash
+++ src/add-word.bash
@@ -1,22 +1,35 @@
 #!/bin/bash
 
-LEXFILE=lexemes/lexemes.tsv
+LEXFILE=lexemes.tsv
 
 if test $# -lt 2 ; then
-    echo "Usage: $0 LEMMA CLASS"
+    echo "Usage: $0 LEMMA CLASS [SOURCE=omorfi]"
     exit 1
+fi
+
+LEMMA=$1
+PARA=$2
+SOURCE=omorfi
+if test $# -ge 3 ; then
+    SOURCE=$3
 fi
 
 cp -v ${LEXFILE} ${LEXFILE}~
 
-case $2 in
-    [0-9]*-[A-Z0]) python3 guess_missing.py --lemma=$1 --ktn=${2%-*} --kav=${2#*-} --output=${LEXFILE} --verbose;;
-    [0-9]*) python3 guess_missing.py --lemma=$1 --ktn=$2 --output=${LEXFILE} --verbose;;
-    [A-Z]*_*) python3 guess_missing.py --lemma=$1 --newpara=$2 --output=${LEXFILE} --verbose;;
-esac
+# OBS! kots tn guessing was lost in github migration??
 
-sort -k1,1 ${LEXFILE} | LC_ALL=C uniq > ${LEXFILE}~~
+HN=1
+if egrep "^$1" ${LEXFILE} | fgrep $2 ; then
+    HN=$(egrep "^$1" ${LEXFILE} | fgrep $2 | tail -n 1 | cut -f 2)
+    HN=$(($HN + 1))
+fi
+printf "%s\t%s\t%s\t%s\n" $LEMMA $HN $PARA $SOURCE >> ${LEXFILE}
+head -n 1 < ${LEXFILE} > ${LEXFILE}~~
+tail -n +2 < ${LEXFILE} | sort -k1,1  | LC_ALL=C uniq >> ${LEXFILE}~~
 diff -u ${LEXFILE}~ ${LEXFILE}~~
-cp ${LEXFILE}~~ ${LEXFILE}
+mv ${LEXFILE}~~ ${LEXFILE}
 echo Abovementioned addition was made, old version is still in ${LEXFILE}~
-echo if all is fine, do commit the result immediately
+echo if all is fine, do commit the result immediately:
+echo $'\t'git commit lexemes.tsv
+echo to roll back changes:
+echo $'\t'mv lexemes.tsv~ lexemes.tsv
