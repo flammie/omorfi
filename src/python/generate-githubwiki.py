4eeca119ef08e71412663695a70e256660f5f153
Flammie Pirinen <flammie@iki.fi>
2015-09-03 18:16:05 +0100

    towards generating documentations

--- src/python/generate-githubwiki.py
+++ src/python/generate-githubwiki.py
@@ -28,6 +28,8 @@ import csv
 
 from omorfi.omor_formatter import format_stuff_omor
 from omorfi.ftb3_formatter import format_stuff_ftb3
+from omorfi.giella_formatter import format_stuff_giella
+from omorfi.apertium_formatter import format_stuff_apertium
 
 # standard UI stuff
 
@@ -45,6 +47,8 @@ def main():
             metavar="PDFILE", help="read paradigm docs from PDFILEs")
     ap.add_argument("--stuff-docs", "-S", action="append", required=True,
             metavar="SDFILE", help="read stuff docs from SDFILE")
+    ap.add_argument("--paradigms", "-A", required=True,
+            metavar="PARADIR", help="read paradigm data from PARADIR/")
     ap.add_argument("--version", "-V", action="version")
     ap.add_argument("--output", "-o", action="store", required=True, 
             type=argparse.FileType('w'),
@@ -63,17 +67,23 @@ def main():
 
     quoting = csv.QUOTE_NONE
     quotechar = None
-    # write header to XML file
+    #
+    # write preamble to wiki page
     print('# Introduction', file=args.output)
     print("""This document was automatically generated from the omorfi 
             database. It describes some of the internal codes used in the
-            database keys and values "_stuff_", and the _paradigms_ that
-            define inflection patterns""", file=args.output)
+            database. Keys and values "_stuff_", and the _paradigms_ that
+            define inflection patterns or so.""", file=args.output)
     # read from csv files
     print('## Stuff ', file=args.output)
     print("""Stuff are internal things, but they appear in database a lot, so
             you will want to know what they are if you are gonna modify database
-            of affixes""", file=args.output)
+            of affixes.""", file=args.output)
+    print("""The stuff is typically used by the file format and/or analysis
+        generators to either define analysis tags or decide whether or not to
+        include the affected string into language model. The default renditions
+        for a handful of omorfi tag formats are provided (only ones that have
+        trivially0 mapped formatting are included.""", file=args.output)
     for tsv_filename in args.stuff_docs:
         if args.verbose:
             print("Reading from", tsv_filename)
@@ -96,7 +106,21 @@ def main():
                         file=args.output)
                 print("* ftb3: ", format_stuff_ftb3(tsv_parts['stuff']),
                         file=args.output)
-    print('## Paradigms', file=args.output)
+                print("* apertium-fin: ", format_stuff_apertium(tsv_parts['stuff']),
+                        file=args.output)
+                print("* giella: ", format_stuff_giella(tsv_parts['stuff']),
+                        file=args.output)
+    
+    paradata = dict()
+    with open(args.paradigms + "/morphophonology.tsv") as tsv_file:
+        tsv_reader = csv.DictReader(tsv_file, delimiter=args.separator,
+                strict=True)
+        for tsv_parts in tsv_reader:
+            paradata[tsv_parts['new_para']] = dict()
+            for key in tsv_parts.keys():
+                if key != 'new_para':
+                    paradata[tsv_parts['new_para']][key] = tsv_parts[key]
+    print('\n## Paradigms', file=args.output)
     for tsv_filename in args.paradigm_docs:
         if args.verbose:
             print("Reading from", tsv_filename)
@@ -113,6 +137,14 @@ def main():
                 print("### `", tsv_parts['new_para'], "`", 
                         file=args.output)
                 print(tsv_parts['doc'], file=args.output)
+                if tsv_parts['new_para'] in paradata:
+                    for key in paradata[tsv_parts['new_para']].keys():
+                        print("* ", key, ": ", paradata[tsv_parts['new_para']][key],
+                                sep='', file=args.output)
+                else:
+                    if not tsv_parts['doc']:
+                        print("UNDOCUMENTED", tsv_parts['new_para'])
+                        exit(1)
     print('''<!-- vim: set ft=markdown:-->''', file=args.output)
     exit()
 
