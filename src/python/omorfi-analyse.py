f16fba969ae0198f274f0f6a21a0d1f15c2f2904
Flammie Pirinen <flammie@iki.fi>
2015-10-13 03:12:43 +0100

    more UD comp. parsing

--- src/python/omorfi-analyse.py
+++ src/python/omorfi-analyse.py
@@ -23,6 +23,134 @@ def get_last_feat(feat, anal):
         rv = feat.group(1)
     return rv
 
+def get_last_feats(anal):
+    re_feats = re.compile("\[[^]]*\]")
+    rvs = list()
+    feats = re_feats.finditer(anal[0].output)
+    for feat in feats:
+        if 'BOUNDARY=' in feat.group(0) or 'WORD_ID=' in feat.group(0):
+            rvs = list()
+        else:
+            rvs.append(feat.group(0))
+    return rvs
+
+
+def format_feats_ud(anal):
+    feats = get_last_feats(anal)
+    rvs = dict()
+    for f in feats:
+        key = f.split("=")[0].lstrip("[")
+        value = f.split("=")[1].rstrip("]")
+        if key == 'CASE':
+            rvs['Case'] = value[0] + value[1:].lower()
+        elif key == 'NUM':
+            if value == 'SG':
+                rvs['Number'] = 'Sing'
+            elif value == 'PL':
+                rvs['Number'] = 'Plur'
+        elif key == 'TENSE':
+            if 'PRESENT' in value:
+                rvs['Tense'] = 'Pres'
+            elif 'PAST' in value:
+                rvs['Tense'] = 'Past'
+        elif key == 'MOOD':
+            rvs['Mood'] = value[0] + value[1:].lower()
+        elif key == 'VOICE':
+            rvs['Voice'] = value[0] + value[1:].lower()
+        elif key == 'PERS':
+            rvs['VerbForm'] = 'Fin'
+            if 'SG' in value:
+                rvs['Number'] = 'Sing'
+            elif 'PL' in value:
+                rvs['Number'] = 'Sing'
+            if '1' in value:
+                rvs['Person'] = '1'
+            elif '2' in value:
+                rvs['Person'] = '2'
+            elif '3' in value:
+                rvs['Person'] = '3'
+        elif key == 'POSS':
+            rvs['VerbForm'] = 'Fin'
+            if 'SG' in value:
+                rvs['Number[psor]'] = 'Sing'
+            elif 'PL' in value:
+                rvs['Number[psor]'] = 'Sing'
+            if '1' in value:
+                rvs['Person[psor]'] = '1'
+            elif '2' in value:
+                rvs['Person[psor]'] = '2'
+            elif '3' in value:
+                rvs['Person[psor]'] = '3'
+        elif key == 'NEG':
+            if value == 'CON':
+                rvs['Connegative'] = 'Yes'
+            elif value == 'NEG':
+                rvs['Negative'] = 'Yes'
+        elif key == 'PCP':
+            rvs['VerbForm'] == 'Inf'
+            if value == 'VA':
+                rvs['PartForm'] = 'Pres'
+            elif value == 'NUT':
+                rvs['PartForm'] = 'Past'
+            elif value == 'MA':
+                rvs['PartForm'] = 'Agent'
+            elif value == 'MATON':
+                rvs['PartForm'] = 'Neg'
+        elif key == 'INF':
+            rvs['VerbForm'] == 'Inf'
+            if value == 'A':
+                rvs['InfForm'] = '1'
+            elif value == 'E':
+                rvs['InfForm'] = '2'
+            elif value == 'MA':
+                rvs['InfForm'] = '3'
+            elif value == 'MINEN':
+                rvs['InfForm'] = '4'
+            elif value == 'MAISILLA':
+                rvs['InfForm'] = '5'
+        elif key == 'CMP':
+            if value == 'SUP':
+                rvs['degree'] = 'Sup'
+            elif value == 'CMP':
+                rvs['degree'] = 'Cmp'
+        elif key == 'SUBCAT':
+            if value == 'NEG':
+                rvs['Negative'] = 'Yes'
+            else:
+                print("Unhandled subcat: ", value)
+                exit(1)
+        elif key in ['UPOS', 'ALLO', 'WEIGHT', 'CASECHANGE', 'LEX', 'GUESS']:
+            continue
+        else:
+            print("Unhandled key", key, '=', value)
+            exit(1)
+    rv = ''
+    for k,v in rvs.items():
+        rv += k + '=' + v + '|'
+    return rv.rstrip('|')
+
+def format_upos_tdt(upos):
+    if upos in ['NOUN', 'PROPN']:
+        return 'N'
+    elif upos == 'ADJ':
+        return 'A'
+    elif upos == 'VERB':
+        return 'V'
+    elif upos in ['CONJ', 'SCONJ']:
+        return 'C'
+    elif upos == 'ADP':
+        return 'Adp'
+    elif upos == 'ADV':
+        return 'Adv'
+    elif upos == 'PRON':
+        return 'Pron'
+    elif upos == 'PUNCT':
+        return 'Punct'
+    elif upos == 'SYM':
+        return 'Symb'
+    else:
+        return 'X'
+
 def print_analyses(wordn, surf, anals, format, outfile):
     if format == 'xerox':
         print_analyses_xerox(surf, anals, outfile)
@@ -89,7 +217,9 @@ def print_analyses_conllx(wordn, surf, anals, outfile):
 
 def print_analyses_conllu(wordn, surf, anals, outfile):
     print(wordn, surf, "#".join(get_lemmas(anals[0])), 
-            get_last_feat("UPOS", anals[0]), "-", anals[0].output,
+            get_last_feat("UPOS", anals[0]), 
+            format_upos_tdt(get_last_feat("UPOS", anals[0])),
+            format_feats_ud(anals),
             "-", "-", "-", "-", sep="\t", file=outfile)
 
 def main():
