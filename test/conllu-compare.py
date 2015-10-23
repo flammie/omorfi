71f9e2055d9fe80a948226fa59637f41fe280442
Flammie Pirinen <flammie@iki.fi>
2015-10-24 01:35:31 +0200

    realign only if asked

--- test/conllu-compare.py
+++ test/conllu-compare.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 """
-Test sort | uniq -c | sort -nr'd gold analysis data for faithfulness
+Compare two conllu files for matches on each field.
 """
 
 
@@ -11,14 +11,16 @@ from sys import stderr, stdin, stdout
 
 def main():
     a = ArgumentParser()
-    a.add_argument('-i', '--input', metavar="INFILE", type=open, required=True,
-            dest="infile", help="analysis results")
-    a.add_argument('-r', '--reference', metavar="INFILE", type=open,
+    a.add_argument('-H', '--hypothesis', metavar="HYPFILE", type=open, required=True,
+            dest="hypfile", help="analysis results")
+    a.add_argument('-r', '--reference', metavar="REFFILE", type=open,
             required=True,
             dest="reffile", help="reference data")
-    a.add_argument('-o', '--output', metavar="OUTFILE", required=True,
+    a.add_argument('-l', '--log', metavar="LOGFILE", required=True,
             type=FileType('w'),
-            dest="outfile", help="result file")
+            dest="logfile", help="result file")
+    a.add_argument('-X', '--realign', action="store_true", default=False,
+            help="Allow fuzzy matches if tokenisation differs")
     a.add_argument('-v', '--verbose', action="store_true", default=False,
             help="Print verbosely while processing")
     options = a.parse_args()
@@ -36,69 +38,74 @@ def main():
     missed_deps2 = 0
     missed_misc = 0
 
-    for inline in options.infile:
+    for hypline in options.hypfile:
         refline = next(options.reffile)
         lines += 1
-        infields = inline.strip().split('\t')
+        infields = hypline.strip().split('\t')
         reffields = refline.strip().split('\t')
         if len(infields) < 4:
-            if 'doc-name' in inline:
+            if 'doc-name' in hypline:
                 continue
-            elif 'sentence-text' in inline:
-                while inline != refline:
+            elif 'sentence-text' in hypline:
+                while hypline != refline:
                     refline = next(options.reffile)
                 continue
-            elif inline == refline:
+            elif hypline == refline:
                 continue
             else:
-                print("mismatched unknown non-content! IN:", inline, "REF:", refline,
+                print("mismatched unknown non-content! IN:", hypline, "REF:", refline,
                         sep='\n')
                 exit(1)
         if infields[0] != reffields[0]:
             skiplines += 1
             print("misaligned (index)! IN:", infields[0], "REF:", reffields[0],
-                    "\n", inline, refline, "skipping...", file=stderr)
-            while inline != "":
-                skiplines += 1
-                inline = next(options.infile).strip()
-            while refline != "":
-                refline = next(options.reffile).strip()
-            continue
+                    "\n", hypline, refline, "skipping...", file=stderr)
+            if options.realign:
+                while hypline != "":
+                    skiplines += 1
+                    hypline = next(options.hypfile).strip()
+                while refline != "":
+                    refline = next(options.reffile).strip()
+                continue
+            else:
+                exit(1)
         if infields[1] != reffields[1]:
             skiplines += 1
             print("misaligned (surface)! IN:", infields[1], "REF:", reffields[1],
-                    "\n", inline, "\n", refline, file=stderr)
-            while inline != "":
-                skiplines += 1
-                inline = next(options.infile).strip()
-            while refline != "":
-                refline = next(options.reffile).strip()
-            continue
-
+                    "\n", hypline, "\n", refline, file=stderr)
+            if options.realign:
+                while hypline != "":
+                    skiplines += 1
+                    hypline = next(options.hypfile).strip()
+                while refline != "":
+                    refline = next(options.reffile).strip()
+                continue
+            else:
+                exit(1)
         if infields[2] != reffields[2]:
             missed_lemmas += 1
-            print("LEMMA", infields[2], reffields[2], file=options.outfile)
+            print("LEMMA", infields[2], reffields[2], file=options.logfile)
         if infields[3] != reffields[3]:
             missed_uposes += 1
-            print("UPOS", infields[3], reffields[3], file=options.outfile)
+            print("UPOS", infields[3], reffields[3], file=options.logfile)
         if infields[4] != reffields[4]:
             missed_tdtposes += 1
-            print("TDTPOS", infields[4], reffields[4], file=options.outfile)
+            print("TDTPOS", infields[4], reffields[4], file=options.logfile)
         if infields[5] != reffields[5]:
             missed_feats += 1
-            print("UFEAT", infields[5], reffields[5], file=options.outfile)
+            print("UFEAT", infields[5], reffields[5], file=options.logfile)
         if infields[6] != reffields[6]:
             missed_uds += 1
-            print("UD", infields[6], reffields[6], file=options.outfile)
+            print("UD", infields[6], reffields[6], file=options.logfile)
         if infields[7] != reffields[7]:
             missed_udlabs += 1
-            print("UDLAB", infields[7], reffields[7], file=options.outfile)
+            print("UDLAB", infields[7], reffields[7], file=options.logfile)
         if infields[8] != reffields[8]:
             missed_deps2 += 1
-            print("DEPS2", infields[8], reffields[8], file=options.outfile)
+            print("DEPS2", infields[8], reffields[8], file=options.logfile)
         if infields[9] != reffields[9]:
             missed_misc += 1
-            print("MISC", infields[9], reffields[9], file=options.outfile)
+            print("MISC", infields[9], reffields[9], file=options.logfile)
         deplines += 1
 
     print("Lines", "Lemma", "UPOS", "UFEAT", "TDT POS", "UD →", "UD LAB",
