9281b150f5935108acfa9866d829f237ad15f5cd
Flammie Pirinen <flammie@iki.fi>
2015-12-02 08:09:01 +0100

    hacks over hacks to experiment...

--- src/python/omorfi-segment.py
+++ src/python/omorfi-segment.py
@@ -5,6 +5,52 @@ from argparse import ArgumentParser
 from omorfi.omorfi import Omorfi
 import re
 
+def print_segments(fmt, segments, labelsegments, surf):
+    if fmt == 'labels-moses-factors':
+        if labelsegments:
+            # öykkärö[UPOS=VERB]i[TRAILS=→][MB=LEFT]dä[TRAILS=→Vpss][?=Tpres][?=Ppe4][?=Ncon][TRAILS=→] 
+            analysis = labelsegments[0].input
+            analysis = analysis.replace("{STUB}", "")
+            # lets do it state-machine way
+            intag = False
+            stacktag = []
+            currtag = ''
+            currmorph = ''
+            carryovertag = None
+            outs = ''
+            for c in analysis:
+                if not intag and c != '[':
+                    currmorph += c
+                elif not intag and c == '[':
+                    intag = True
+                elif intag and c != ']':
+                    currtag += c
+                elif intag and c == ']':
+                    if currtag.startswith('UPOS'):
+                        carryovertag = currtag
+                    elif currmorph != '' and carryovertag:
+                        outs += currmorph + '|' +  carryovertag + ' '
+                        carryovertag = None
+                        currmorph = ''
+                    elif currmorph != '':
+                        outs += currmorph + '|' +  currtag + ' '
+                        currmorph = ''
+                    elif currmorph == '':
+                        outs += '.' + currtag + ' '
+                    currtag = ''
+                    intag = False
+            print(outs.replace("TRAILS=→", "",).replace("UPOS=", "").replace(" .", ".").replace("?=", "").replace("MB=LEFT", "").replace("..", ".").replace(". ", " "))
+        else:
+            print(surf, end='|UNK')
+    elif fmt == 'both-lines':
+        if segments and labelsegments:
+            print(segments[0].input, labelsegments[0].input, end=' ', file=outfile)
+        if segments:
+            print(segments[0].input, end=' ', file=outfile)
+        if labelsegments:
+            print(labelsegments[0].input, end=' ', file=outfile)
+
+
 def main():
     """Segment text in some formats."""
     a = ArgumentParser()
@@ -16,6 +62,9 @@ def main():
             help="print verbosely while processing")
     a.add_argument('-o', '--output', metavar="OUTFILE", 
             help="print segments into OUTFILE")
+    a.add_argument('-O', '--output-format', metavar="OFORMAT", 
+            help="format output suitable for OFORMAT",
+            choices=["labels-tsv", "labels-moses-factors", "segments-arrows"])
     options = a.parse_args()
     omorfi = Omorfi(options.verbose)
     if options.fsa:
@@ -52,12 +101,7 @@ def main():
         for surf in surfs:
             segments = omorfi.segment(surf)
             labelsegments = omorfi.labelsegment(surf)
-            if segments and labelsegments:
-                print(segments[0].input, labelsegments[0].input, end=' ', file=outfile)
-            if segments:
-                print(segments[0].input, end=' ', file=outfile)
-            if labelsegments:
-                print(labelsegments[0].input, end=' ', file=outfile)
+            print_segments(options.output_format, segments, labelsegments, surf)
         print(file=outfile)
     exit(0)
 
