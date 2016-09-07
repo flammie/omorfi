49265530ec92dbbaac77ba0ecbdbd8b30a4bbcef
Tommi A Pirinen <tommi.antero.pirinen@uni-hamburg.de>
2016-09-07 18:51:03 +0200

    minor updates to helper scripts here

--- src/bash/generate-analysedlist.bash
+++ src/bash/generate-analysedlist.bash
@@ -5,7 +5,7 @@ if test $# -lt 2 ; then
 fi
 cat $1 | while read l; do
     echo ${l}...
-    echo -n \"${l} \"  >> $2
+    echo -n \"${l}\"  >> $2
     echo ${l} |\
         sed -e 's/./\0 /g' |\
         sed -e 's/[$_]/%\0/g' |\
@@ -14,9 +14,6 @@ cat $1 | while read l; do
         hfst-regexp2fst -o src/generated/temporary.wordform.hfst
     hfst-compose -v -F src/generated/temporary.omor.hfst src/generated/temporary.wordform.hfst -o src/generated/temporary.wordgen.hfst
     hfst-fst2strings src/generated/temporary.wordgen.hfst |\
-        sed -e 's/\\:/@@@/g' |\
-        cut -d : -f 1 |\
-        sed -e 's/@@@/:/g' |\
         uniq |\
         tr -s '\n' ' ' >> $2
     echo >> $2
