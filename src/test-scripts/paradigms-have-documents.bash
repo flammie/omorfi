c7d58dc4e3701f117b97ad9b32c1d12a35013ee3
Flammie Pirinen <flammie@iki.fi>
2017-08-07 04:59:56 +0200

    comm is weird

--- src/test-scripts/paradigms-have-documents.bash
+++ src/test-scripts/paradigms-have-documents.bash
@@ -6,10 +6,12 @@ WORK=$(mktemp -d -t omorfi-validate-database.XXXXXXXXXX)
 echo checking for missing docs for paradigms in ${LEXFILE}...
 cut -f 3 ${LEXFILE} | sort | uniq > ${WORK}/paradigms
 cut -f 1 ${DOCFILE} | sort | uniq > ${WORK}/docs.paradigms
-comm -23 ${WORK}/docs.paradigms ${WORK}/paradigms > ${WORK}/missing-para-docs
+comm -23 ${WORK}/paradigms ${WORK}/docs.paradigms > ${WORK}/missing-para-docs
 while read k ; do
     echo MISSING $k is found in ${LEXFILE} but is not in $DOCFILE >> ${WORK}/fails.paradigms
-    fgrep -- "${k}	" $LEXFILE | fgrep 'omorfi++' >> ${WORK}/fails.paradigms
+    fgrep -m 1 -- "${k}	" $LEXFILE |\
+        awk '{printf("%s\tfor example: %s\n", $3, $1);}' \
+        >> ${WORK}/fails.paradigms
     echo ADD $k into ${DOCFILE} >> ${WORK}/fails.paradigms
 done < ${WORK}/missing-para-docs
 if test -e ${WORK}/fails.paradigms ; then
