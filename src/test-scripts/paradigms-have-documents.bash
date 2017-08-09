e9a55a916325ef21f47369561408d9481881f5c8
Flammie Pirinen <flammie@iki.fi>
2017-08-09 03:52:09 +0200

    start paradigm docs

--- src/test-scripts/paradigms-have-documents.bash
+++ src/test-scripts/paradigms-have-documents.bash
@@ -10,10 +10,15 @@ comm -23 ${WORK}/paradigms ${WORK}/docs.paradigms > ${WORK}/missing-para-docs
 while read k ; do
     echo MISSING $k is found in ${LEXFILE} but is not in $DOCFILE >> ${WORK}/fails.paradigms
     fgrep -m 1 -- "${k}	" $LEXFILE |\
-        awk '{printf("%s\tfor example: %s\n", $3, $1);}' \
+        awk -F '\t' '{printf("%s\tfor example: %s\n", $3, $1);}' \
         >> ${WORK}/fails.paradigms
     echo ADD $k into ${DOCFILE} >> ${WORK}/fails.paradigms
 done < ${WORK}/missing-para-docs
+comm -13 ${WORK}/paradigms ${WORK}/docs.paradigms > ${WORK}/extra-para-docs
+while read k ; do
+    echo EXTRA $k is found in ${DOCFILE} but is not in $LEXFILE >> ${WORK}/fails.paradigms
+    echo REMOVE $k from ${DOCFILE} >> ${WORK}/fails.paradigms
+done < ${WORK}/extra-para-docs
 if test -e ${WORK}/fails.paradigms ; then
     echo
     echo there are missing docs in ${DOCFILE}, see ${WORK}/fails.paradigms
