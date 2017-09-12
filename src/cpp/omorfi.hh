6cb1896c97fe94b0d108b63e35d29c9c89c2ba4a
Flammie Pirinen <flammie@iki.fi>
2017-09-12 09:55:33 +0200

    docs for xygen

--- src/cpp/omorfi.hh
+++ src/cpp/omorfi.hh
@@ -27,9 +27,14 @@
 
 #include <hfst/hfst.h>
 
-
+/**
+ * Namespace for omorfi stuff.
+ */
 namespace omorfi {
 
+    /**
+     * An object that can load and use omorfi language models.
+     */
     class Omorfi {
 
     private:
@@ -41,23 +46,74 @@ namespace omorfi {
 
     public:
 
-        Omorfi();
+      /** Construct omorfi with no models loaded. */
+      Omorfi();
 
-        ~Omorfi();
+      /** Destroy omorfi and unload all models. */
+      ~Omorfi();
 
-        void loadAllFromDefaultDirs();
+      /**
+       * load all omorfi models found in the default directories:
+       *
+       * - $prefix/share/omorfi
+       * - $HOME/.omorfi
+       * - ./src/generated
+       * - ./generated
+       * - .
+       *
+       * @deprecated This is a convenience function and is indefinetely 
+       * deprecated. Use specific loading functions instead.
+       */
+      void loadAllFromDefaultDirs();
 
-        void loadFromDir(const std::string& path);
+      /**
+       * load all omorfi models from a given directory. 
+       *
+       * @deprecated This relies on standard filenames, which can change
+       * between versions. This convenience function is deprecated, it is
+       * preferred to use specific loading functions instead.
+       */
+      void loadFromDir(const std::string& ath);
 
-        void loadFile(const std::string& filename);
+      /**
+       * load an omorfi model from a file.
+       *
+       * @deprecated Associating model to right functionality relies on
+       * standard filenames, that change between versions, this convenience
+       * function is deprecated. Use specific loading functions instead.
+       */
+      void loadFile(const std::string& filename);
 
-        void loadAnalyser(const std::string& filename);
+      /**
+       * load analysis model from a file.
+       */
+      void loadAnalyser(const std::string& filename);
 
-        std::vector<std::string> analyse(const std::string& token);
+      /**
+       * analyse a string as a single input token.
+       *
+       * @return an unordered vector of strings giving all known analyses. May
+       * be an empty list if no analyses are found, but this behaviour cannot
+       * be trusted upon: many models will back-off giving at least one analysis
+       * for any given input.
+       */
+      std::vector<std::string> analyse(const std::string& token);
 
-        std::vector<std::string> tokenise(const std::string& text);
+      /**
+       * tokenise a string for analysis.
+       *
+       * @return a vector of tokens given as strings, in order they should
+       * appear in the analyses. The return value does not need to be a
+       * split of  source @c text.
+       */
+      std::vector<std::string> tokenise(const std::string& text);
 
-        bool accept(const std::string& token);
+      /**
+       * Test if string is a valid word-form and in the lexicon.
+       * Note that this function is not any faster than @c analyse, but it
+       * resolves some internal codings of guessed analyses.
+       */
+      bool accept(const std::string& token);
 
 
     };
