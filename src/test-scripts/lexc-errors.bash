ce3ac587d138322e112aecfda4fc1023e70fca3a
Tommi A Pirinen <tommi.antero.pirinen@uni-hamburg.de>
2018-04-04 14:04:46 +0200

    add new symbols for monodix

--- src/test-scripts/lexc-errors.bash
+++ src/test-scripts/lexc-errors.bash
@@ -3,4 +3,10 @@ if fgrep ERRORMACRO generated/omorfi.lexc ; then
     echo lexc is broken
     exit 1
 fi
+if test -f generated/omorfi.dix ; then
+    if fgrep '<s n="ERROR"' generated/omorfi.dix ; then
+        echo dix is broken
+        exit 1
+    fi
+fi
 exit 0
