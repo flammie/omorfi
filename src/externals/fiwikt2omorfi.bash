64777037ae7dd749d5debb7b66ef11827dee52fb
Tommi A Pirinen <tommi.antero.pirinen@uni-hamburg.de>
2018-07-03 18:58:28 +0200

    working with fiwikt a bit

--- src/externals/fiwikt2omorfi.bash
+++ src/externals/fiwikt2omorfi.bash
@@ -17,17 +17,17 @@ fi
 
 # Define word class
 wc='(Substantiivi|Adjektiivi|Pronomini|Numeraali|Prepositio|Adverbi|Interjektio|Konjunktio|Partikkeli|Verbi|Erisnimi)'
-# Fetch only relevant lines from the xml dump (NOTE: This assumes relevant 
+# Fetch only relevant lines from the xml dump (NOTE: This assumes relevant
 # lines are between <page> & </page> tags)
 cat $@ | sed -ne '/<page>/,/<\/page>/p' |\
 # Remove all line-initial whitespaces
     sed -e 's/^[ \t]*//g' |\
 # Remove unwanted xml tags
     sed -ne '/\(<page>\|<title>\)/p' -ne '/<text/,/<\/page>/p' |\
-# Remove unwanted xml tag (NOTE: The </revision> tag is found between </text> 
+# Remove unwanted xml tag (NOTE: The </revision> tag is found between </text>
 # & </page>)
     sed '/<\/revision>/d' |\
-# Remove linebreaks 
+# Remove linebreaks
     tr -d '\n' |\
 # Place linebreak infront of each <page>
     sed -re "s/<page>/\n\0/g" |\
@@ -86,7 +86,50 @@ cat $@ | sed -ne '/<page>/,/<\/page>/p' |\
     fgrep -v '<entry>' |\
     tr '|' ',' |\
     sed -re 's/([[:digit:]]+)-([[:upper:]])/\1,\2/' |\
-    gawk -F , 'NF == 3 {printf("\"%s\",\"%s\",\"0\",\"%s\"\n", $1, $2, $3);} NF == 4 {printf("\"%s\",\"%s\",\"%s\",\"%s\"\n", $1, $2, $3, $4);}' |\
-    sed -e 's/Substantiivi/N/' -e 's/Adjektiivi/A/' -e 's/Numeraali/Num/' \
-        -e 's/Verbi/V/' -e 's/Erisnimi/Prop/' -e 's/Pronomini/Pron/' \
-        -e 's/Adverbi/Adv/' 
+    gawk -F , 'NF == 3 {printf("%s\t1\t%s_%s\tfiwikt\n", $1, $3, $2);}
+        NF == 4 {printf("%s\t1\t%s_%s%s\tfiwikt\n", $1, $4, $2, $3);}' |\
+    sed -e 's/Adverbi_99/ADV_NOPEASTI/' \
+        -e 's/\(o	1	\)Erisnimi_1	/\1PROPN_TALO	/' \
+        -e 's/\(u	1	\)Erisnimi_1	/\1PROPN_ASU	/' \
+        -e 's/\(ö	1	\)Erisnimi_1	/\1PROPN_MÖMMÖ	/' \
+        -e 's/\(o	1	\)Erisnimi_1A	/\1PROPN_UKKO	/' \
+        -e 's/\(y	1	\)Erisnimi_1C	/\1PROPN_PYTTY	/' \
+        -e 's/\(o	1	\)Erisnimi_1E	/\1PROPN_VETO	/' \
+        -e 's/\(o	1	\)Erisnimi_1F	/\1PROPN_VETO	/' \
+        -e 's/\(o	1	\)Erisnimi_1I	/\1PROPN_KIELTO	/' \
+        -e 's/\(o	1	\)Erisnimi_1J	/\1PROPN_TUNTO	/' \
+        -e 's/\(o	1	\)Erisnimi_1K	/\1PROPN_SIIRTO	/' \
+        -e 's/\(o	1	\)Erisnimi_2	/\1PROPN_RUIPELO	/' \
+        -e 's/\(o	1	\)Erisnimi_3	/\1PROPN_TUOMIO	/' \
+        -e 's/\([äöyÄÖY].*i	1	\)Erisnimi_5	/\1PROPN_TYYLI	/' \
+        -e 's/\(i	1	\)Erisnimi_5	/\1PROPN_RUUVI	/' \
+        -e 's/\([äöyÄÖY].*i	1	\)Erisnimi_5A	/\1PROPN_HÄKKI	/' \
+        -e 's/\([äöyÄÖY].*i	1	\)Erisnimi_6	/\1PROPN_KÄNÄÄLI	/' \
+        -e 's/\(.*i	1	\)Erisnimi_6	/\1PROPN_KANAALI	/' \
+        -e 's/\(i	1	\)Erisnimi_5A	/\1PROPN_LOKKI	/' \
+        -e 's/\(i	1	\)Erisnimi_7	/\1PROPN_ONNI	/' \
+        -e 's/\(i	1	\)Erisnimi_7D	/\1PROPN_NOKI	/' \
+        -e 's/\(i	1	\)Erisnimi_7E	/\1PROPN_LAHTI	/' \
+        -e 's/\(i	1	\)Erisnimi_7F	/\1PROPN_KORPI	/' \
+        -e 's/\([äöy].*i	1	\)Erisnimi_7D	/\1PROPN_KÄKI	/' \
+        -e 's/\(a	1	\)Erisnimi_9	/\1PROPN_KIRJA	/' \
+        -e 's/\(a	1	\)Erisnimi_9I	/\1PROPN_VALTA	/' \
+        -e 's/\(a	1	\)Erisnimi_9J	/\1PROPN_KUTSUNTA	/' \
+        -e 's/\(a	1	\)Erisnimi_9K	/\1PROPN_KERTA	/' \
+        -e 's/\(a	1	\)Erisnimi_10	/\1PROPN_VOIMA	/' \
+        -e 's/\(ä	1	\)Erisnimi_10	/\1PROPN_HÖPÖTTÄJÄ	/' \
+        -e 's/\(a	1	\)Erisnimi_10E	/\1PROPN_LUPA	/' \
+        -e 's/\(ä	1	\)Erisnimi_11	/\1PROPN_HÄKKYRÄ	/' \
+        -e 's/\(a	1	\)Erisnimi_12	/\1PROPN_KITARA	/' \
+        -e 's/\(a	1	\)Erisnimi_18	/\1PROPN_MAA	/' \
+        -e 's/\(ä	1	\)Erisnimi_18	/\1PROPN_PÄÄ	/' \
+        -e 's/\(ie	1	\)Erisnimi_19	/\1PROPN_TIE	/' \
+        -e 's/\(i	1	\)Erisnimi_24	/\1PROPN_RUUHI	/' \
+        -e 's/\(i	1	\)Erisnimi_25	/\1PROPN_TAIMI	/' \
+        -e 's/\(i	1	\)Erisnimi_26	/\1PROPN_RUUHI	/' \
+        -e 's/\([äöyÄÖY].*nen	1	\)Erisnimi_38	/\1PROPN_KYLKIÄINEN	/' \
+        -e 's/\(nen	1	\)Erisnimi_38	/\1PROPN_AAKKOSTAMINEN	/' \
+        -e 's/\(us	1	\)Substantiivi_39	/\1NOUN_VAKUUTUS	/' \
+        -e 's/\(as	1	\)Erisnimi_41G	/\1PROPN_KANGAS	/' \
+        -e 's/\(e	1	\)Erisnimi_48J	/\1PROPN_RAKENNE	/' \
+        -e 's/\(.	1	\)Erisnimi_51	/\1PROPN_51XXX	/'
