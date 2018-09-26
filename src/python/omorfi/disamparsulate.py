1ac84720e702286e2a66a0b44c7c7d9a776aa22d
Flammie Pirinen <flammie@iki.fi>
2018-09-26 10:55:13 +0200

    sketchy sketch

--- src/python/omorfi/disamparsulate.py
+++ src/python/omorfi/disamparsulate.py
@@ -15,34 +15,63 @@ class Suggestion:
 
     def __init__(self):
         self.name = "Unnamed"
+        self.target = None
         # ± tokens = stuff
         self.context = None
+        # barrier for * contexts
+        self,barrier = None
         # when 0 is also
         self.dispute = None
         # dep to add
         self.depname = None
         # from:
-        # ~inf: quasi-always (grammatical, CG style) to
-        # 1.0: weak evidence, last resort tie break
-        self.likelihood = 1.0
-
-    def matches(self, token: Token, sentence: list):
-        '''If suggestion applies to token in context.'''
-        return False
+        # -inf: quasi-always (grammatical, CG style SELECT) to
+        # -1.0: weak evidence, last resort tie break
+        # 1.0: unlikely
+        # inf: nearly never (ungrammatical, CG REMOVE)
+        self.unlikelihood = -1.0
 
     def apply(self, token: Token, sentence: list):
-        '''Draw an arc and redistribute some weights.'''
-        return
+        '''If suggestion applies to token in context.'''
+        for analysis in token.analyses:
+            matched = True
+            if "ufeats" in self.target:
+                for feat, value in self.target["ufeats"].items():
+                    if feat not in analysis.ufeats:
+                        matched = False
+                        break
+                    elif analysis.ufeats[feat] != value:
+                        matched = False
+                        break
+                if not matched:
+                    continue
+            if self.context:
+                head = self.find_context(token, sentence)
+                if not head:
+                    matched = False
+                    continue
+            if matched:
+                if self.unlikelihood > 0:
+                    analysis.weight += self.unlikelihood
+                elif self.unlikelihood < 0:
+                    for b in token.analyses:
+                        if b != analysis:
+                            b.weight += self.unlikelihood
+                if head and self.dep:
+                    analysis.ud = head
 
 
 def linguisticate(sentence: list):
     '''Not a parsing function.'''
     #
     rules = list()
+    insrule = Suggestion()
+    insrule.name = "instructives unlikely"
+    insrule.target = {"ufeats": {"Case": "Ins"}}
+    insrule.unlikelihood = 16.0
     for token in sentence:
         for rule in rules:
-            if rule.matches(token, sentence):
-                rule.apply(token, sentence)
+            rule.apply(token, sentence)
 
 
 def main():
