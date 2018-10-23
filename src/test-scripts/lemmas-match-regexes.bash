b4f0c077a31b027d70734c8f2db1059959861c42
Tommi A Pirinen <tommi.antero.pirinen@uni-hamburg.de>
2018-10-23 17:49:20 +0200

    alpha test download funxn

--- src/test-scripts/lemmas-match-regexes.bash
+++ src/test-scripts/lemmas-match-regexes.bash
@@ -1,6 +1,6 @@
 #!/bin/bash
 if ! ${srcdir}/test-scripts/lemmas-match-regexes.py \
-        --input ${srcdir}/generated/master.tsv; then
+        --input generated/master.tsv; then
     exit 1
 fi
 exit 0
