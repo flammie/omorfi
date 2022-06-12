6a40fec503c4f5964c75a0d15617686378441331
Flammie Pirinen <flammie@iki.fi>
2022-06-13 01:27:32 +0200

    version bump
    had to remove hfst from hard deps as hfst on pip does not install
    anymore

--- src/python/setup.py
+++ src/python/setup.py
@@ -1,11 +1,13 @@
+#!/usr/bin/env python3
+"""Python setup for some pypi stuff?"""
 import setuptools
 
-with open("README.md", "r") as fh:
+with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="omorfi", # Replace with your own username
-    version="0.0.2",
+    name="omorfi",
+    version="0.9.9",
     author="Flammie A Pirinen",
     author_email="flammie@iki.fi",
     description="Open morphology for Finnish, python bindings",
@@ -19,20 +21,20 @@ setuptools.setup(
         "Operating System :: OS Independent",
     ],
     keywords="finnish nlp morphology",
-    python_requires='>=3.5',
-    scripts=['omorfi-download.py',
-             'omorfi-conllu.py',
-             'omorfi-convert.py',
-             'omorfi-disamparsulate.py',
-             'omorfi-download.py',
-             'omorfi-factorise.py',
-             'omorfi-freq-evals.py',
-             'omorfi-ftb3.py',
-             'omorfi-segment.py',
-             'omorfi-sigmorphons.py',
-             'omorfi-tokenise.py',
-             'omorfi-unimorph.py',
-             'omorfi-vislcg.py',
-             'omorfi-wikitable.py'],
-    install_requires=['hfst'],
+    python_requires=">=3.5",
+    scripts=["omorfi-download.py",
+             "omorfi-conllu.py",
+             "omorfi-convert.py",
+             "omorfi-disamparsulate.py",
+             "omorfi-download.py",
+             "omorfi-factorise.py",
+             "omorfi-freq-evals.py",
+             "omorfi-ftb3.py",
+             "omorfi-segment.py",
+             "omorfi-sigmorphons.py",
+             "omorfi-tokenise.py",
+             "omorfi-unimorph.py",
+             "omorfi-vislcg.py",
+             "omorfi-wikitable.py"],
+    install_requires=["hfst"],
 )
