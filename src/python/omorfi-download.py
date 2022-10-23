d112240da4fdf496cbbb86653cd741adb4598ac2
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-23 11:19:43 +0000

    Adding tarfile member sanitization to extractall()

--- src/python/omorfi-download.py
+++ src/python/omorfi-download.py
@@ -43,7 +43,29 @@ def main():
     with tarfile.open(mode="r|xz", fileobj=io.BytesIO(r.content)) as z:
         if args.verbose:
             print("unpackin...")
-        z.extractall()
+        
+        import os
+        
+        def is_within_directory(directory, target):
+            
+            abs_directory = os.path.abspath(directory)
+            abs_target = os.path.abspath(target)
+        
+            prefix = os.path.commonprefix([abs_directory, abs_target])
+            
+            return prefix == abs_directory
+        
+        def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+        
+            for member in tar.getmembers():
+                member_path = os.path.join(path, member.name)
+                if not is_within_directory(path, member_path):
+                    raise Exception("Attempted Path Traversal in Tar File")
+        
+            tar.extractall(path, members, numeric_owner=numeric_owner) 
+            
+        
+        safe_extract(z)
 
 
 if __name__ == "__main__":
